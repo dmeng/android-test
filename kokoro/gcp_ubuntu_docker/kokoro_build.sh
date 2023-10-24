71b258dcba2a194690443eaa16f1e30343fd8d09
Dennis Meng <dennismeng@google.com>
2023-10-24 16:34:28 -0700

    see what versions are available and try 17.0.7 since the latest appears to fail

--- kokoro/gcp_ubuntu_docker/kokoro_build.sh
+++ kokoro/gcp_ubuntu_docker/kokoro_build.sh
@@ -11,7 +11,9 @@ cd "${KOKORO_ARTIFACTS_DIR}/github/android-test-releases"
 /opt/android-sdk/cmdline-tools/latest/bin/sdkmanager --sdk_root=/opt/android-sdk "build-tools;33.0.2"
 /opt/android-sdk/cmdline-tools/latest/bin/sdkmanager --sdk_root=/opt/android-sdk "platforms;android-34" "build-tools;34.0.0"
 
-apt-get install -y openjdk-17-jdk
+apt-get update
+apt-cache madison openjdk-17
+apt-get install -y openjdk-17-jdk=17.0.7+7
 export JAVA_HOME="$(update-java-alternatives -l | grep "1.17" | head -n 1 | tr -s " " | cut -d " " -f 3)"
 
 cd gradle-tests

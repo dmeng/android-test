a7b974a614ebd2423afc01d11799da118c3e6d97
Dennis Meng <dennismeng@google.com>
2023-10-25 10:49:25 -0700

    set swiftshader_indirect

--- kokoro/gcp_ubuntu_docker/kokoro_build.sh
+++ kokoro/gcp_ubuntu_docker/kokoro_build.sh
@@ -5,19 +5,11 @@ set -e
 
 cd "${KOKORO_ARTIFACTS_DIR}/github/android-test-releases"
 
-# runner has @androidsdk//:legacy_test-30 as a dependency
-/opt/android-sdk/cmdline-tools/latest/bin/sdkmanager --sdk_root=/opt/android-sdk "platforms;android-30" "build-tools;30.0.0"
-# building :axt_m2_repository uses @androidsdk//:build-tools/33.0.2/aapt2
-/opt/android-sdk/cmdline-tools/latest/bin/sdkmanager --sdk_root=/opt/android-sdk "build-tools;33.0.2"
-/opt/android-sdk/cmdline-tools/latest/bin/sdkmanager --sdk_root=/opt/android-sdk "platforms;android-34" "build-tools;34.0.0"
-
 curl -s https://repos.azul.com/azul-repo.key | sudo gpg --dearmor -o /usr/share/keyrings/azul.gpg
 echo "deb [signed-by=/usr/share/keyrings/azul.gpg] https://repos.azul.com/zulu/deb stable main" | sudo tee /etc/apt/sources.list.d/zulu.list
 apt-get update
 apt-get install -y zulu17-jdk
 export JAVA_HOME="$(update-java-alternatives -l | grep "1.17" | head -n 1 | tr -s " " | cut -d " " -f 3)"
 
-if [[ -e /dev/kvm ]] && grep '^flags' /proc/cpuinfo | grep -qE 'vmx|svm'; then echo 'KVM is working'; else echo 'KVM not working'; fi
-
 cd gradle-tests
-./gradlew nexusOneApi30DebugAndroidTest --info
+./gradlew nexusOneApi30DebugAndroidTest --info -Pandroid.testoptions.manageddevices.emulator.gpu=swiftshader_indirect

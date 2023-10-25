0ef9660d0345c4f64413403ae7f4f79d51dffaa7
Dennis Meng <dennismeng@google.com>
2023-10-25 09:51:36 -0700

    try using zulu jdk instead of openjdk

--- kokoro/gcp_ubuntu_docker/kokoro_build.sh
+++ kokoro/gcp_ubuntu_docker/kokoro_build.sh
@@ -11,8 +11,10 @@ cd "${KOKORO_ARTIFACTS_DIR}/github/android-test-releases"
 /opt/android-sdk/cmdline-tools/latest/bin/sdkmanager --sdk_root=/opt/android-sdk "build-tools;33.0.2"
 /opt/android-sdk/cmdline-tools/latest/bin/sdkmanager --sdk_root=/opt/android-sdk "platforms;android-34" "build-tools;34.0.0"
 
+curl -s https://repos.azul.com/azul-repo.key | sudo gpg --dearmor -o /usr/share/keyrings/azul.gpg
+echo "deb [signed-by=/usr/share/keyrings/azul.gpg] https://repos.azul.com/zulu/deb stable main" | sudo tee /etc/apt/sources.list.d/zulu.list
 apt-get update
-apt-get install -y openjdk-17-jdk
+apt-get install -y zulu17-jdk
 export JAVA_HOME="$(update-java-alternatives -l | grep "1.17" | head -n 1 | tr -s " " | cut -d " " -f 3)"
 
 cd gradle-tests

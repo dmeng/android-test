166bd679573a372c537654abf4d649b6df9e5650
Dennis Meng <dennismeng@google.com>
2023-10-25 09:36:01 -0700

    see if the problem was because of the missing apt-get update

--- kokoro/gcp_ubuntu_docker/kokoro_build.sh
+++ kokoro/gcp_ubuntu_docker/kokoro_build.sh
@@ -12,8 +12,7 @@ cd "${KOKORO_ARTIFACTS_DIR}/github/android-test-releases"
 /opt/android-sdk/cmdline-tools/latest/bin/sdkmanager --sdk_root=/opt/android-sdk "platforms;android-34" "build-tools;34.0.0"
 
 apt-get update
-apt-cache showpkg openjdk-17-jdk
-apt-get install -y openjdk-17-jdk=17.0.8.1+1~us1-0ubuntu1~20.04
+apt-get install -y openjdk-17-jdk
 export JAVA_HOME="$(update-java-alternatives -l | grep "1.17" | head -n 1 | tr -s " " | cut -d " " -f 3)"
 
 cd gradle-tests

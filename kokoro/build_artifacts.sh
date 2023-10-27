acdcd803e40366a563922e956b9a1fbd48c73237
Dennis Meng <dennismeng@google.com>
2023-10-27 12:54:55 -0700

    move scripts into single file

--- kokoro/build_artifacts.sh
+++ kokoro/build_artifacts.sh
@@ -17,5 +17,15 @@ install_bazelisk
 
 bazelisk build :axt_m2repository
 
-# copy the zip here so that we don't follow symlinks to get the files to upload
 cp bazel-bin/axt_m2repository.zip .
+unzip bazel-bin/axt_m2repository.zip -d /root/.m2
+
+curl -s https://repos.azul.com/azul-repo.key | sudo gpg --dearmor -o /usr/share/keyrings/azul.gpg
+echo "deb [signed-by=/usr/share/keyrings/azul.gpg] https://repos.azul.com/zulu/deb stable main" | sudo tee /etc/apt/sources.list.d/zulu.list
+apt-get update
+apt-get install -y zulu17-jdk
+export JAVA_HOME="$(update-java-alternatives -l | grep "1.17" | head -n 1 | tr -s " " | cut -d " " -f 3)"
+
+cd gradle-tests
+./gradlew nexusOneApi30DebugAndroidTest -Pandroid.testoptions.manageddevices.emulator.gpu=swiftshader_indirect -Dandroid.experimental.androidTest.numManagedDeviceShards=1
+
